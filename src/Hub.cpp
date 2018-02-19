8732e23033cc35bca9cdf031a6e3470a88542392
robotsms <31376455+robotsms@users.noreply.github.com>
2018-02-19 22:56:12 +0800

    Update Hub.cpp
    bug fix.
    if extraHeaders=null,  program will "Segmentation fault (core dumped)

--- src/Hub.cpp
+++ src/Hub.cpp
@@ -177,9 +177,10 @@ void Hub::connect(std::string uri, void *user, std::map<std::string, std::string
                                      "Host: " + hostname + ":" + std::to_string(port) + "\r\n"
                                      "Sec-WebSocket-Version: 13\r\n";
 
-            for (std::pair<std::string, std::string> header : extraHeaders) {
-                httpSocket->httpBuffer += header.first + ": " + header.second + "\r\n";
-            }
+            if(extraHeaders.size())
+                for (std::pair<std::string, std::string> header : extraHeaders) {
+                     httpSocket->httpBuffer += header.first + ": " + header.second + "\r\n";
+                }
 
             httpSocket->httpBuffer += "\r\n";
         } else {
